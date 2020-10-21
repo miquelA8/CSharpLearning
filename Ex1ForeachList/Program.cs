578790c042555eb863171ec5b367c1cebc9d571a
miquelA8 <miquelangellopez@paucasesnovescifp.cat>
2020-10-21 12:34:10 +0200

    Upload first exercice
    List of String Array and one foreach to show.

--- Ex1ForeachList/Program.cs
+++ Ex1ForeachList/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Ex1ForeachList
 {
@@ -10,6 +11,13 @@ namespace Ex1ForeachList
     {
         static void Main(string[] args)
         {
+            var sort = new List<String>() { "Pepe", "Jose", "Antonio", "Federico", "Bernardo" };
+
+            foreach(var value in sort)
+            {
+                Console.WriteLine(value);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
