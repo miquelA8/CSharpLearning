f56e7f6d4ff31e1d7bb4a0a20e98a23479661156
miquelA8 <miquelangellopez@paucasesnovescifp.cat>
2020-10-22 20:15:59 +0200

    First part of Date Time exercice
    This is the first part of Date Time exercice. You can type a day and
    this program tells how many days have been past.

---
+++ Ex2DateTime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var current = DateTime.Now;
+            Console.WriteLine("Give me a date:          (dd-mm-yyyy)");
+            string sDatePrevious = Console.ReadLine();
+            DateTime datePrevious = DateTime.ParseExact(sDatePrevious, "d-M-yyyy", null);
+            TimeSpan totalDate = DateTime.Now.Subtract(datePrevious);
+            Console.WriteLine($"It have been {totalDate.Days} days since {sDatePrevious}");
+            Console.ReadLine();
+            //Console.WriteLine("Give me a time          HH:mm tt ");
+            
+            
+           /* var notCurrent = DateTime.Parse(Console.ReadLine()) ;
+            var totalCurrent = notCurrent - current;
+            var notCurrentTT = notCurrent.ToString("hh:mm tt");
+            var totalCurrentTT = totalCurrent.ToString("hh:mm tt");
+            Console.WriteLine($"{notCurrentTT} was {totalCurrentTT)} ago ");
+            Console.ReadLine();*/
+        }
+    }
+}
